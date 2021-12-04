8f3983e3b53148cd2f75bbc94876899c29b23ca7
TangXuanzhao <xuanzhaotang@gmail.com>
2021-12-04 21:52:40 +0800

    fix compile problem

--- rabbitmq/mq.go
+++ rabbitmq/mq.go
@@ -7,7 +7,6 @@ import (
 	"github.com/glory-go/glory/mq"
 	"github.com/glory-go/glory/tools"
 	"github.com/streadway/amqp"
-	"google.golang.org/appengine/log"
 )
 
 type RabbitMQService struct {
@@ -45,84 +44,4 @@ func (s *RabbitMQService) DelaySend(topic string, msg []byte, handleTime time.Ti
 
 func (s *RabbitMQService) RegisterHandler(topic string, handler mq.MQMsgHandler) {
 	panic("not implement")
-}
-
-func (s *RabbitMQService) send(chName string, msg []byte, delayTime *int64) error {
-	chConfig, ok := s.config.Channels[chName]
-	if !ok {
-		return fmt.Errorf("channel %v not found", chName)
-	}
-	// 检查格式
-	if chConfig.Type == Delay || chConfig.Type == PubSub {
-		if chConfig.ExchangeName == "" {
-			return fmt.Errorf("invalid channel %v, delay queue must has exchange name", chName)
-		}
-	}
-	// 建立连接
-	ch, err := s.conn.Channel()
-	if err != nil {
-		return err
-	}
-	defer ch.Close()
-
-	// 初始化queue
-	q, err := ch.QueueDeclare(
-		chConfig.QueueName, // name
-		true,               // durable
-		false,              // delete when unused
-		false,              // exclusive
-		false,              // no-wait
-		nil,                // arguments
-	)
-	if err != nil {
-		log.Errorf("init channel queue %v for %v meets error: %v", chConfig.QueueName, chName, err)
-		return err
-	}
-
-	// 初始化exchange
-	if chConfig.ExchangeName != "" {
-		args := amqp.Table{}
-		// TODO: 未考虑direct模式下应该如何定义
-		exchangeType := "fanout"
-		if chConfig.Type == Delay {
-			args["x-delayed-type"] = "direct"
-			exchangeType = "x-delayed-message"
-		}
-		if err := ch.ExchangeDeclare(
-			chConfig.ExchangeName, // name
-			exchangeType,          // type
-			true,                  // durable
-			false,                 // auto-deleted
-			false,                 // internal
-			false,                 // no-wait
-			args,                  // arguments
-		); err != nil {
-			log.Errorf("init channel exchange %v for %v meets error", chConfig.ExchangeName, chName)
-			return err
-		}
-	}
-
-	headers := amqp.Table{}
-	routingKey := q.Name
-	if chConfig.Type == Delay {
-		if delayTime == nil {
-			tmp := int64(0)
-			delayTime = &tmp
-		}
-		headers["x-delay"] = *delayTime
-		routingKey = ""
-	}
-	if err = ch.Publish(
-		chConfig.ExchangeName, // exchange
-		routingKey,            // routing key
-		false,                 // mandatory
-		false,                 // immediate
-		amqp.Publishing{
-			Headers:     headers,
-			ContentType: "text/plain",
-			Body:        msg,
-		}); err != nil {
-		return err
-	}
-	return nil
 }
