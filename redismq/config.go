fe97d1855bc71d1c65ced73465b158b11f3ac4bc
LEEzanhui <43463965+LEEzanhui@users.noreply.github.com>
2022-01-17 18:14:26 +0800

    change BuckCnt & DB type

--- redismq/config.go
+++ redismq/config.go
@@ -2,11 +2,11 @@ package redismq
 
 type Config struct {
 	QueueName string `yaml:"name"`
-	BuckCnt int `yaml:"buck_cnt"`
+	BuckCnt   string `yaml:"buck_cnt"`
 
 	Host     string `yaml:"host"`
 	Port     string `yaml:"port"`
 	Username string `yaml:"username"`
 	Password string `yaml:"password"`
-	DB       int    `yaml:"db"`
+	DB       string    `yaml:"db"`
 }
